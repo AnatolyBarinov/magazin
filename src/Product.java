0b68a28d3b79ecc4dc118fbee4797457957f15e2
AnatolyBarinov <62007944+AnatolyBarinov@users.noreply.github.com>
2023-10-30 08:26:53 +0300

    //Избегание магических чисел // //В классе "Product" нет использования магических чисел. // Класс содержит поля с понятными именами, такими как "price", "rating" и другие, которые не зависят // от конкретного числового значения.

---
+++ src/Product.java
@@ -0,0 +1,46 @@
+
+public class Product {
+    private String name;
+    private String manufacturer;
+    private double price;
+    private int rating;
+
+    public Product(String name, String manufacturer, double price, int rating) {
+        this.name = name;
+        this.manufacturer = manufacturer;
+        this.price = price;
+        this.rating = rating;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+}
